8f6c2d046c215dc70ff5ff3c4a3af9f5a9d4f22e
Micah Mutrux <micah.mutrux@gmail.com>
2019-03-04 16:51:18 -0500

    first test route

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the express library here
+const express = require("express");
+
+// Instantiate the app here
+const app = express();
+
+// Invoke the app's `.listen()` method below:
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+// stub out first route & response
+const moods = [{ mood: "excited about express!" }, { mood: "route-tastic!" }];
+app.get("/moods", (req, res, next) => {
+  res.send(moods);
+});
